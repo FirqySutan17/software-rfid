9f20f0c1603a59616ecce7acdf7cdc607b974f87
cjichsan <m.ichsan@cj.co.id>
2024-01-12 16:17:50 +0700

    Server Update

--- app/Http/Controllers/PlacementController.php
+++ app/Http/Controllers/PlacementController.php
@@ -8,6 +8,13 @@ class PlacementController extends Controller
 {
     public function index($rack_no, Request $request) {
     	dd($rack_no);
+    	$cold_storage 	= $rack_no[0].$rack_no[1];
+    	$rack_position	= $rack_no[2].$rack_no[3];
+    	$sequence 		= $rack_no[4];
+
+    	$rack_data = [
+    		"cold_storage" => 
+    	];
         return view('placement.index');
     }
 
